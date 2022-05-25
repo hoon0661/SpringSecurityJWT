0f1db211911a92b700957ee69a1319cb43402dcf
Hoon Kim <hoon0661@gmail.com>
2022-05-25 18:14:50 -0400

    initial UserService

---
+++ src/main/java/com/example/springsecurityjwt/service/UserServiceImpl.java
@@ -0,0 +1,49 @@
+package com.example.springsecurityjwt.service;
+
+import com.example.springsecurityjwt.domain.Role;
+import com.example.springsecurityjwt.domain.User;
+import com.example.springsecurityjwt.repo.RoleRepo;
+import com.example.springsecurityjwt.repo.UserRepo;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+@Slf4j
+public class UserServiceImpl implements UserService{
+
+    private final UserRepo userRepo;
+    private final RoleRepo roleRepo;
+
+    @Override
+    public User saveUser(User user) {
+        return userRepo.save(user);
+    }
+
+    @Override
+    public Role saveRole(Role role) {
+        return roleRepo.save(role);
+    }
+
+    @Override
+    public void addRoleToUser(String username, String roleName) {
+        User user = userRepo.findByUsername(username);
+        Role role = roleRepo.findByName(roleName);
+        user.getRoles().add(role);
+    }
+
+    @Override
+    public User getUser(String username) {
+        return userRepo.findByUsername(username);
+    }
+
+    @Override
+    public List<User> getUsers() {
+        return userRepo.findAll();
+    }
+}
