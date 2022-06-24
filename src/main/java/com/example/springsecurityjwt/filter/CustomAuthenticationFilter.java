cf41ddb80d619023cf888b58f9b6ccef6d4b9489
hoon <hoon0661@gmail.com>
2022-06-24 10:00:41 -0400

    Generate JWT tokens

--- src/main/java/com/example/springsecurityjwt/filter/CustomAuthenticationFilter.java
+++ src/main/java/com/example/springsecurityjwt/filter/CustomAuthenticationFilter.java
@@ -1,10 +1,14 @@
 package com.example.springsecurityjwt.filter;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.servlet.FilterChain;
@@ -12,6 +16,8 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Date;
+import java.util.stream.Collectors;
 
 @Slf4j
 public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
@@ -44,7 +50,26 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
     //Using that, we can pass headers, and response body, etc.
     //In this project, we will pass a token using that response.
     @Override
-    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
-        super.successfulAuthentication(request, response, chain, authResult);
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) throws IOException, ServletException {
+        User user = (User)authentication.getPrincipal(); // returns user that is successfully authenticated
+
+        //Now, create JWT
+        Algorithm algorithm = Algorithm.HMAC256("secret".getBytes()); //Algorithm to use. In real world, need to make it more complex and encrypt the secret.
+        String access_token = JWT.create()
+                .withSubject(user.getUsername()) // use something unique about the user as a subject
+                .withExpiresAt(new Date(System.currentTimeMillis() + 10 * 60 * 1000)) //10 minutes
+                .withIssuer(request.getRequestURL().toString()) // author, or company of token
+                .withClaim("roles", user.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList())) //add roles
+                .sign(algorithm);
+
+        String refresh_token = JWT.create()
+                .withSubject(user.getUsername()) // use something unique about the user as a subject
+                .withExpiresAt(new Date(System.currentTimeMillis() + 30 * 60 * 1000)) //30 minutes
+                .withIssuer(request.getRequestURL().toString()) // author, or company of token
+                .sign(algorithm); // no need to pass roles
+
+        //put tokens in header
+        response.setHeader("access_token", access_token);
+        response.setHeader("refresh_token", refresh_token);
     }
 }
