45a80bc4a0d9d54ebf47ac2af09b1b76442ba504
hoon <hoon0661@gmail.com>
2022-06-24 10:18:42 -0400

    added implementation for token generation

--- src/main/java/com/example/springsecurityjwt/filter/CustomAuthenticationFilter.java
+++ src/main/java/com/example/springsecurityjwt/filter/CustomAuthenticationFilter.java
@@ -2,6 +2,7 @@ package com.example.springsecurityjwt.filter;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -17,7 +18,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.Collectors;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @Slf4j
 public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
@@ -68,8 +73,18 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
                 .withIssuer(request.getRequestURL().toString()) // author, or company of token
                 .sign(algorithm); // no need to pass roles
 
-        //put tokens in header
+        /*
+        // put tokens in header
         response.setHeader("access_token", access_token);
         response.setHeader("refresh_token", refresh_token);
+        */
+
+        //or send this in response body in JSON format
+        Map<String, String> tokens = new HashMap<>();
+        tokens.put("access_token", access_token);
+        tokens.put("refresh_token", refresh_token);
+        response.setContentType(APPLICATION_JSON_VALUE);
+        new ObjectMapper().writeValue(response.getOutputStream(), tokens);
     }
+
 }
